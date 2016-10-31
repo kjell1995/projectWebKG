6d89a221c62bb113595ebfb54e61533a544fb602
Kjell Deboysere <Kjell Deboysere>
2016-10-31 14:36:48 +0100

    made simple home page and made some error pages

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+const app = express();
+
+app.set("port", process.env.PORT || 80);
+app.engine("handlebars", handlebars({
+  defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//home page
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404);
+  res.render("404");
+});
+
+// 500 page
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  res.render("500");
+});
+
+app.listen(app.get("port"), () => {
+  console.log("Server started on http://localhost: " + app.get("port"));
+});
